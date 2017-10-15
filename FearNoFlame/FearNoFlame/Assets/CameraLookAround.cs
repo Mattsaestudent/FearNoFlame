38b2d19f91ea0eb52d118fd2ebec1bd555d95e28
matt donnelly <mattpdonnelly@yahoo.com.au>
2017-10-15 22:20:25 +0800

    Scolling for the camera

---
+++ FearNoFlame/FearNoFlame/Assets/CameraLookAround.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAround : MonoBehaviour {
+
+    public float panSpeed = 4f;
+    public float panBorderThickness = 10f;
+
+
+    public Vector2 panLimit;
+
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness )
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+       
+     
+        pos.x = Mathf.Clamp(pos.x, 21.8f, 29.5f);
+
+        pos.z = Mathf.Clamp(pos.z, 9.9f, 24.4f);
+        transform.position = pos;
+    }
+
+
+}
