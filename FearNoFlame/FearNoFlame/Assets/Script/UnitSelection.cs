6e43fe83cd8ce0d700cf64dd869f9ce63a866e2d
matt donnelly <mattpdonnelly@yahoo.com.au>
2017-10-27 05:26:17 +0800

    A*Star

---
+++ FearNoFlame/FearNoFlame/Assets/Script/UnitSelection.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class UnitSelection : MonoBehaviour {
+
+    public GameObject highlightedPlayer;
+    public GameObject SelectedPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //Debug.Log("Mouse is over: " + hitInfo.collider.name );
+
+            // The collider we hit may not be the "root" of the object
+            // You can grab the most "root-est" gameobject using
+            // transform.root, though if your objects are nested within
+            // a larger parent GameObject (like "All Units") then this might
+            // not work.  An alternative is to move up the transform.parent
+            // hierarchy until you find something with a particular component.
+
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+
+            SelectObject(hitObject);
+        }
+        else
+        {
+            ClearSelection();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            SelectedPlayer = highlightedPlayer;
+
+
+        }
+      
+
+          
+                
+          
+            
+        
+    }
+
+    void SelectObject(GameObject obj)
+    {
+        if (highlightedPlayer != null)
+        {
+            if (obj == highlightedPlayer)
+                return;
+
+            ClearSelection();
+        }
+
+        highlightedPlayer = obj;
+
+        Renderer[] rs = highlightedPlayer.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            Material m = r.material;
+            m.color = Color.green;
+            r.material = m;
+        }
+
+
+    }
+
+    void ClearSelection()
+    {
+        if (highlightedPlayer == null)
+            return;
+
+        Renderer[] rs = highlightedPlayer.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            Material m = r.material;
+            m.color = Color.white;
+            r.material = m;
+        }
+
+
+        highlightedPlayer = null;
+       
+    }
+}
